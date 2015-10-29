55cb7dfca4ad17b33180f5b3a2026c1f768d84d0
Jaysig <jasontsigmon@gmail.com>
2015-10-29 14:24:46 -0400

    Index & 404 tests passing

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,8 @@
+var finalhandler = require('finalhandler');
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var serveStatic = require('serve-static');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -10,12 +12,36 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+exports.serveIndex = serveStatic(__dirname + '/public', { 'index' : 'index.html'});
+
 exports.serveAssets = function(res, asset, callback) {
+
+
+
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
 };
 
+exports.sendResponse = function(response, data, statusCode){
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, exports.headers);
+  response.end(JSON.stringify(data));
+};
 
+exports.makeActionHandler = function(actionMap){
+  var actionMap = actionMap;
+  return function(request, response) {
+    var action = actionMap[request.method];
+    if (action) {
+      action(request, response);
+    } else {
+      exports.sendResponse(response, '', 404);
+    }
+  }
+};
+
+//Router to serve files
+//Use archive helpers
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
+var finalhandler = require('finalhandler')
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelp = require('./http-helpers.js')
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var actionFunction = httpHelp.makeActionHandler(actions);
+  actionFunction(req,res);
+  //res.end(archive.paths.list);
+};
+
+var actions = {
+  'GET': function(req, res){
+    var done = finalhandler(req, res);
+    httpHelp.serveIndex(req, res,done);
+  },
+  'POST': function(req, res){
+  },
+  'OPTIONS': function(req, res){
+    // httpHelp.sendResponse(res, null);
+  }
 };
